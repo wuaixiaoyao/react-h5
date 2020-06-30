81131b4e0bffb8b4febdcd12b3fc2e9a93c4dc47
yll <yangll-h@glodon.com>
2020-06-30 09:50:29 +0800

    feat: 添加socketIo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,21 +2,38 @@
  * @author wuaixiaoyao
  * @date 2019/9/23
  * @Description:
-*/
+ */
 import React from 'react';
-import { renderRoutes } from 'react-router-config'
+import {
+  renderRoutes
+} from 'react-router-config'
+import io from 'socket.io'
 import Example from '@/components/hooks/exmap'
 import WordAdder from '@/components/example/pureComponent'
-const Home = ({ route }) => (
+const Home = ({
+  route
+}) => {
   //todo;待完善
-  <div>
-    <h2>我是Home组件</h2>
-    <Example/>  
-    {/* 测试 浅比较 */}
-    <WordAdder></WordAdder> 
-    {renderRoutes(route.routes, { someProp: 'these extra props are optional' })}
+  const soc = io('http://localhost:7002');
+  //向指定的服务器建立连接，地址可以省略
+  console.log('soc', soc);
+  soc.emit('msg', '你好服务器');
+  //自定义msg事件，发送‘你好服务器’字符串向服务器
+  soc.on('msg', (data) => {
+    //监听浏览器通过msg事件发送的信息
+    console.log(data); //你好浏览器
+  });
+  debugger
+  return (
+    <div>
+      <h2>我是Home组件</h2>
+      <Example/>  
+      {/* 测试 浅比较 */}
+      <WordAdder></WordAdder> 
+  
+      {renderRoutes(route.routes, { someProp: 'these extra props are optional' })}
 
-  </div>
-
-);
+    </div>
+  )
+};
 export default Home
